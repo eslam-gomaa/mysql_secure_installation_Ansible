3a972a57212d989b5165b04da8a4b9e023bda3dd
eslam-gomaa <eslam@Home>
2020-07-08 13:11:50 +0200

    Remove unix_socket if exists (to disable passwordless authentication)

--- library/mysql_secure_installation.py
+++ library/mysql_secure_installation.py
@@ -235,6 +235,12 @@ def mysql_secure_installation(login_password, new_password, user='root',login_ho
         try:
             connection = mysql.connect(host=login_host, user=user, passwd=login_password, db='mysql')
             cursor = connection.cursor()
+
+            cursor.execute("SELECT host, user, password, plugin FROM mysql.user where User='{}' LIMIT 0,1;".format(user))
+            socket_exists = cursor.fetchall()
+            if 'unix_socket' in list(chain.from_iterable(socket_exists)):
+                cursor.execute("UPDATE mysql.user SET plugin = '' WHERE user = {};".format(user))
+
             remove_anon_user(cursor)
             remove_testdb(cursor)
             disallow_root_remotely(cursor)
