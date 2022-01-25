702c3ffea9bb6dbc4d8bd26db0226e7e5a8622e3
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-01-25 12:25:05 +0000

    add test commands

--- test_scripts/ubuntu_18_04.sh
+++ test_scripts/ubuntu_18_04.sh
@@ -7,10 +7,10 @@ cd mysql_secure_installation_Ansible
 echo "This will Install & Secure MySQL"
 ansible-playbook sample-playbook.yml
 
-# echo "Check that the user password changed"
-# cat output.txt | grep 'Password for user' | grep  'changed to the desired state'
+echo "Check that the user password changed"
+cat output.txt | grep 'Password for user' | grep  'changed to the desired state'
 
-# echo "Run the playbook again"
-# ansible-playbook sample-playbook.yml
-# echo "Check that the user password will NOT change the next time"
-# cat output.txt | grep 'Password of' | grep  'Already meets the desired state'
+echo "Run the playbook again"
+ansible-playbook sample-playbook.yml
+echo "Check that the user password will NOT change the next time"
+cat output.txt | grep 'Password of' | grep  'Already meets the desired state'
