6bb1ceb9fd17e5fa3593f3dd6dd46557b8ab8cf3
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-01-25 21:44:45 +0000

    Addansible installation scripts

---
+++ test_scripts/install_ansible_debian10.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+apt install ansible -y
---
+++ test_scripts/install_ansible_fedora34.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf install ansible git -y
